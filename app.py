b711cececa91d83f7844d12a2e2703a0ed8b3e04
yawmoth32 <theroce86@gmail.com>
2025-11-27 16:45:14 -0300

    Change app port from 5000 to 10000

--- app.py
+++ app.py
@@ -773,4 +773,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], safe_filename)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=10000)
